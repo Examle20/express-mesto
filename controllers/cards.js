428035bbff312f66dfb048f7192357a68a107501
engineer <Examle20@yandex.ru>
2021-04-18 16:48:41 +0300

    feat: сделать обработку ошибок для карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,17 @@
 const Card = require('../models/card');
 
+module.exports.doesCardExist = (req, res, next) => {
+  Card.findById(req.params.cardId)
+    .then(card => {
+      if(!card) {
+        res.status(404).send({message: 'Карточка с указанным _id не найдена.'})
+        return
+      }
+    })
+    .catch(() => {res.status(500).send({ message: 'Произошла ошибка' })});
+  next();
+};
+
 module.exports.getCards = (req, res) => {
   console.log(req.user._id); // _id станет доступен
   Card.find({})
@@ -13,13 +25,19 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then(card => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) =>{
+      if(err.name === 'ValidationError'){
+        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки'});
+      }else {
+        res.status(500).send({ message: err});
+      }
+    })
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
     .then(()=> res.send({ message: 'Успешно'}))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
@@ -29,7 +47,13 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
 )
   .populate('likes')
   .then((card)=> res.send({likes: card.likes}))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  .catch((err) =>{
+    if(err.name === 'TypeError' || err.name ==='CastError'){
+      res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка'});
+    }else {
+      res.status(500).send({ message: err});
+    }
+  })
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
@@ -37,4 +61,10 @@ module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   { new: true },
 )
   .then(()=> res.send({ message: 'Успешно'}))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  .catch((err) =>{
+    if(err.name === 'TypeError' || err.name ==='CastError'){
+      res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка'});
+    }else {
+      res.status(500).send({ message: err});
+    }
+  })
